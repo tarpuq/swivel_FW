1dc70c7eb555fe8fdb910559b999cda45e0cbadb
Galo Guzmán <galo.guzman@outlook.com>
2019-10-18 09:57:22 -0500

    sTimer.c to sTimers.c

---
+++ turnstile.X/sTimers.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   sTimers.c
+ * Author:      gguzman
+ * Comments:
+ * Revision history: 
+ */
+#include <stdint.h>
+
+#include "sTimers.h"
+
+uint8_t readerTimeout = 0;
+uint8_t readerTimeoutFlag = 0;
+
+void softwareTimersHandler(void)
+{
+    if(readerTimeout){
+        readerTimeout--;
+        
+        if(readerTimeout == 0)
+            readerTimeoutFlag = 1;
+    }
+}
